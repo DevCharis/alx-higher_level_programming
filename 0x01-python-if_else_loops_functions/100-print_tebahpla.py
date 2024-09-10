4721cfc698185745750c97fc4b29e36f4ba7e173
devcharis <joshuabamidele9@gmail.com>
2024-09-10 12:51:17 +0100

    advanced type sh12

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{:c}".format(122 - i + (i % 2) * 32), end="")
-for i in range(26):
-    char = chr(122 - i)  # Start from 'z' (122) and go to 'a'
     if i % 2 == 0:
-        print(char, end="")
+        print(chr(122 - i), end="")  # Lowercase
     else:
-        print(char.upper(), end="")
+        print(chr(90 - i), end="")    # Uppercase
