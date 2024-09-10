e06b578948ac87363caac1e2e4ee8129a0cbf23f
devcharis <joshuabamidele9@gmail.com>
2024-09-10 12:56:19 +0100

    advanced type sh12

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{:c}".format(122 - i) if i % 2 == 0 else "{:c}".format(90 - i), end="")
+    print("{:c}".format(122 - i)
+          if i % 2 == 0
+          else "{:c}".format(90 - i), end="")
