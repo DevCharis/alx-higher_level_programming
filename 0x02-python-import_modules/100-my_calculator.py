45aca36f22b19c036072ac6c9c0873b5fbcc47d7
devcharis <joshuabamidele9@gmail.com>
2024-09-12 14:35:57 +0100

    Importer, exporter, debbuger3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-if operator == '+':
-    print(f"{a} {operator} {b} = {add(a, b)}")
-elif operator == '-':
-    print(f"{a} {operator} {b} = {sub(a, b)}")
-elif operator == '*':
-    print(f"{a} {operator} {b} = {mul(a, b)}")
-elif operator == '/':
-    print(f"{a} {operator} {b} = {div(a, b)}")
-else:
-    print("Unknown operator. Available operators: +, -, * and /\n")
-    sys.exit(1)
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = operations[operator](a, b)
+    print(f"{a} {operator} {b} = {result}")
