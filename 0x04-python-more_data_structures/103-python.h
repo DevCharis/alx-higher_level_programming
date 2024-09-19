c0bb336af0fd48bb2e908374d1d97fb52a989743
devcharis <joshuabamidele9@gmail.com>
2024-09-19 22:33:42 +0100

    c in python, or is it the other way around, i don pus tire today, i suppose don born :)

--- 0x04-python-more_data_structures/103-python.h
+++ 0x04-python-more_data_structures/103-python.h
@@ -1,9 +1,9 @@
-#ifndef 103_PYTHON_H
-#define 103_PYTHON_H
+#ifndef PYTHON_H
+#define PYTHON_H
 
 #include <Python.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
-#endif /* 103_PYTHON_H */
+#endif /* PYTHON_H */
