e8c7ceeb1144bb7a0470afed68524ac2030d4fe7
devcharis <joshuabamidele9@gmail.com>
2024-09-19 22:43:17 +0100

    omo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,42 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
-
-/**
- * is_palindrome - Checks if a linked list is a palindrome.
- * @head: Pointer to the head of the list.
- * Return: 1 if palindrome, 0 otherwise.
- */
 int is_palindrome(listint_t **head)
 {
 listint_t *slow = *head, *fast = *head;
 listint_t *prev = NULL, *second_half;
-
 if (*head == NULL || (*head)->next == NULL)
 return 1;
-
-// Use fast and slow pointers to find the middle of the list
 while (fast && fast->next)
 {
-listint_t *next_slow = slow->next; // Store next node of slow
-slow->next = prev; // Reverse the first half
-prev = slow; // Move prev one step forward
-slow = next_slow; // Move slow one step forward
+listint_t *next_slow = slow->next;
+slow->next = prev;
+prev = slow;
+slow = next_slow;
 fast = fast->next->next;
 }
-
-// If the number of nodes is odd, skip the middle node
 if (fast)
 slow = slow->next;
-
-second_half = slow; // The second half starts from here
-
-// Compare the reversed first half with the second half
+second_half = slow;
 int result = compare_lists(prev, second_half);
-
-// Restore the original list (optional)
 reverse_list(prev);
-
 return result;
 }
--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
@@ -9,21 +8,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
-
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-
-    return (n);
+const listint_t *current;
+unsigned int n; /* number of nodes */
+current = h;
+n = 0;
+while (current != NULL)
+{
+printf("%i\n", current->n);
+current = current->next;
+n++;
 }
-
+return (n);
+}
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer to pointer of first node of listint_t list
@@ -32,54 +28,24 @@ size_t print_listint(const listint_t *h)
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new;
-    listint_t *current;
-
-    current = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
+listint_t *new;
+listint_t *current;
+current = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+*head = new;
+else
+{
+while (current->next != NULL)
+current = current->next;
+current->next = new;
 }
-// Function to reverse a linked list
-listint_t *reverse_list(listint_t *head) {
-    listint_t *prev = NULL, *next = NULL;
-
-    while (head != NULL) {
-        next = head->next; // Store next node
-        head->next = prev; // Reverse the link
-        prev = head;       // Move prev up
-        head = next;       // Move head up
-    }
-    return prev; // New head of the reversed list
+return (new);
 }
-
-// Function to compare two linked lists
-int compare_lists(listint_t *list1, listint_t *list2) {
-    while (list1 != NULL && list2 != NULL) {
-        if (list1->n != list2->n) {
-            return 0; // Not a palindrome
-        }
-        list1 = list1->next;
-        list2 = list2->next;
-    }
-    return (list1 == NULL && list2 == NULL) ? 1 : 0; // Both lists should be the same length
-}
-
 /**
  * free_listint - frees a listint_t list
  * @head: pointer to list to be freed
@@ -87,13 +53,11 @@ int compare_lists(listint_t *list1, listint_t *list2) {
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+listint_t *current;
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
 }
-
+}
