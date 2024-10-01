ef2929cb3ff2094e9f0067fdeb6bfadc80945d86
devcharis <joshuabamidele9@gmail.com>
2024-10-01 05:12:30 +0100

    omo, exceptions again, debug, advanced type sh11, debug

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 void print_python_list(PyObject *p) {
+   (void)p;
   /* Implement the details for printing Python list information */
 }
 
 void print_python_bytes(PyObject *p) {
+  (void)p;
   /* Implement the details for printing Python bytes information */
 }
 
 void print_python_float(PyObject *p) {
+   (void)p;
   /* Implement the details for printing Python float information */
 }
